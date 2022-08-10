fd7e68b96d309ad4058c39c370bfa5f59ec5e566
pryority <matthewapryor@gmail.com>
2022-08-10 01:54:50 -0400

    feat: start popup ui

--- client/src/components/Map/MainMap.js
+++ client/src/components/Map/MainMap.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import Map, { Source, Layer, Marker } from 'react-map-gl';
+import Map, { Source, Layer, Marker, Popup } from 'react-map-gl';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import { Button } from '@mui/material';
 
 const geojson = {
     type: 'FeatureCollection',
@@ -24,6 +26,10 @@ export default function MainMap() {
         zoom: 15
     });
 
+    const [showPopup, setShowPopup] = useState(true);
+    // const handleMarkerClick = () => {
+    //     setShowPopup(!showPopup)
+    // }
     return (
         <Map
             {...viewState}
@@ -36,15 +42,51 @@ export default function MainMap() {
             <Marker
                 latitude={40}
                 longitude={-81}
-                anchor="bottom"
+                anchor="right"
                 offset={0}
                 scale={2}
             >
-                <div latitude={0} longitude={0} className='font-black text-4xl'>You are here!</div>
+                {/* <div latitude={0} longitude={0} className='font-black text-4xl'>You are here!</div> */}
             </Marker>
-            <Source id="my-data" type="geojson" data={geojson}>
+            {showPopup && (
+                <Popup longitude={-81} latitude={40}
+                    anchor="left"
+                    onClose={() => setShowPopup(false)}
+                    focusAfterOpen={true}
+                >
+                    <div className='w-full'>
+                        <img src='https://www.seguin.ca/en/explore-play/resources/Pictures/11140305_bn_seguin-interiors_0052_bike-on-trail.jpg' alt='trail-demo' className='rounded-md mb-2' />
+                        <h4 className='text-xl font-bold'>Brick Works Blitz</h4>
+                        <div className='border-b mb-2' />
+                        <div className='flex space-x-2 items-center text-md space-between justify-between'>
+                            <p className='text-md'>Type:</p>
+                            <p className='font-medium text-slate-900 capitalize text-sm'>Adventure</p>
+                        </div>
+                        <div className='flex space-x-2 items-center text-md space-between justify-between'>
+                            <p className='text-md'>Completions:</p>
+                            <p className='font-medium text-slate-900 capitalize text-sm'>513</p>
+                        </div>
+                        <div className='flex space-x-2 items-center text-md space-between justify-between'>
+                            <p className='text-md'>Distance:</p>
+                            <p className='font-medium text-slate-900 capitalize text-sm'>8 km</p>
+                        </div>
+                        <div className='flex w-full justify-center mt-4'>
+                            <button className='flex w-full items-center justify-center bg-blue-500 border hover:border-slate-300 text-white p-1 px-2 rounded-md'
+                                onClick={() => console.log('viewing course!')}
+                            >
+                                View Course
+                            </button>
+                            <div className='flex items-center px-2'>
+                                <BookmarkIcon color="primary" />
+                            </div>
+                        </div>
+
+                    </div>
+                </Popup>)
+            }
+            {/* <Source id="my-data" type="geojson" data={geojson}>
                 <Layer {...layerStyle} />
-            </Source>
-        </Map>
+            </Source> */}
+        </Map >
     )
 }

cdae3983983b6db169d79873af8263cdba43e750
pryority <matthewapryor@gmail.com>
2022-08-22 19:29:07 -0400

    feat: add secondary visual of checkpoints
    on click of checkpoint, map centers to selected checkpoint coordinates

--- client/src/components/Maps/CreateMap.js
+++ client/src/components/Maps/CreateMap.js
@@ -9,19 +9,23 @@ import ControlPointIcon from '@mui/icons-material/ControlPoint';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import AutorenewIcon from '@mui/icons-material/Autorenew';
 import ClearIcon from '@mui/icons-material/Clear';
+import trail from '../../assets/images/trail.png'
+import demo from '../../assets/images/demo.png'
 
 import axios from 'axios';
 const TOKEN = process.env.REACT_APP_MAPBOX;
 
 export default function CreateMap() {
     const [viewState, setViewState] = useState({
-        latitude: 20,
-        longitude: -81,
-        zoom: 5
+        latitude: 49.29837092831224,
+        longitude: -122.93407397752691,
+        zoom: 10.647035651394196
     });
     const [checkpoints, setCheckpoints] = useState([]);
     const [toggle, setToggle] = useState(false);
     const [selectedCP, setSelectedCP] = useState({});
+    const [showIntro, setShowIntro] = useState(true);
+
     const mapRef = useRef();
     const handleMarkerClick = (cp, lat, long) => {
         setSelectedCP(cp);
@@ -44,15 +48,15 @@ export default function CreateMap() {
                 "lat": lat,
                 "long": lng
             })
-            console.log('🟢 COURSES:', res.data)
+            console.log('🟢 CHECKPOINTS:', res.data)
             // setCourses(res.data)
         } catch (err) {
             console.log('<><><><><><><>', err, '<><><><><><><>')
         }
     }
 
-    const handleAddMarker = async (loc) => {
-        console.log('Location:', loc)
+    const handleAddMarker = (loc) => {
+        // console.log('Location:', loc)
         const checkpoint = {
             "username": "<asdf>",
             "title": "<asdf>",
@@ -63,27 +67,30 @@ export default function CreateMap() {
             "long": loc.long
         }
         checkpoints.push(checkpoint)
-        console.log('🟢 COURSES:', checkpoints)
+        console.log('Adding Marker -- UPDATED CHECKPOINTS ARRAY:', checkpoints)
     };
 
     useEffect(() => {
-        const getCourses = async () => {
-            try {
-                const res = await axios.get('/api/new-course')
-                console.log('🟢 COURSES:', res.data)
-                setCheckpoints(res.data)
-            } catch (err) {
-                console.log('<><><><><><><>', err, '<><><><><><><>')
-            }
-        };
-        getCourses();
-    }, [])
+        // const getCourses = async () => {
+        //     try {
+        //         const res = await axios.get('/api/new-course')
+        //         console.log('🟢 CHECKPOINTS:', res.data)
+        //         setCheckpoints(res.data)
+        //     } catch (err) {
+        //         console.log('<><><><><><><>', err, '<><><><><><><>')
+        //     }
+        // };
+        // getCourses();
+        setCheckpoints(checkpoints)
+        console.log(checkpoints)
+        console.log(viewState)
+    }, [viewState, checkpoints])
 
     return (
         <Map
             {...viewState}
             ref={mapRef}
-            onMove={evt => setViewState(evt.viewState)}
+            onMove={(e) => setViewState(e.viewState)}
             mapboxAccessToken={TOKEN}
             // mapStyle="mapbox://styles/matthewpryor/ckzepsxne002b14ov4oo7gb8r"
             mapStyle="mapbox://styles/mapbox/streets-v9"
@@ -91,7 +98,6 @@ export default function CreateMap() {
             minZoom={2}
             onClick={(e) => {
                 handleAddMarker({ lat: e.lngLat.lat, long: e.lngLat.lng })
-
             }}
         >
             {
@@ -143,7 +149,7 @@ export default function CreateMap() {
                                         latitude={marker.lat}
                                         longitude={marker.long}
                                         anchor="bottom"
-                                        closeButton={false}
+                                        closeButton={true}
                                         closeOnClick={false}
                                         offset={[-10, -40]}
                                     >
@@ -167,85 +173,79 @@ export default function CreateMap() {
                 })
             }
 
-            <div id='filter-menu' className='flex w-full justify-center items-center absolute top-4 z-50' >
+            <div id='filter-menu' className='flex w-full justify-center items-center absolute top-4 z-2' >
                 <div className='flex w-5/6 justify-center items-start h-full absolute'>
-                    <div className='flex flex-col space-y-2 w-full items-center bg-slate-200 p-3 rounded-xl border-2 border-slate-800/30 shadow'>
+                    <div className='flex flex-col space-y-2 w-full items-center bg-slate-200/90 p-3 rounded-xl border-2 border-slate-800/30 shadow'>
                         <div
                             className='flex flex-col w-full items-center'
                         >
                             <div className='flex w-full space-x-2'>
-                                <div id='filter'
-                                    onClick={() => {
-                                        console.log('clicked')
-                                        setToggle(!toggle)
-                                        console.log(toggle)
-                                    }}
-                                    className='flex space-x-1 h-full p-2 items-center rounded-lg bg-sky-100 border-2 border-slate-400'>
-                                    <FilterAltIcon fontSize='small' />
-                                    <p className='font-medium text-md text-slate-700'>Filter</p>
-                                    <KeyboardArrowDownIcon fontSize='small' />
-                                </div>
-                                {/* GEOCODER */}
-
-                                <input id='geocode-search' placeholder='Search by location' className='flex  flex-col w-full z-50 rounded-lg p-2 border-2 border-slate-400' />
+                                <input id='geocode-search' placeholder='Search by location' className='flex  flex-col w-full z-2 rounded-lg p-2 border-2 border-slate-400' />
                                 <div className='flex p-2 justify-end'>
                                     <AutorenewIcon fontSize='small' />
                                 </div>
                             </div>
 
-                            {toggle && (<div className='flex flex-col w-full justify-around items-around'>
-                                <div className='grid grid-cols-2 mt-4 items-center gap-2 w-full'>
-                                    <div className='flex w-full flex-col'>
-                                        <p className='text-slate-700 font-medium'>Distance</p>
-                                        <div className='flex border border-slate-400 rounded-lg'>
-                                            <input placeholder='Min' className='flex w-full p-1 px-2 rounded-l-lg' />
-                                            <div className='border' />
-                                            <input placeholder='Max' className='flex w-full p-1 px-2 rounded-r-lg' />
-                                        </div>
-                                    </div>
-
-                                    <div className='flex w-full flex-col'>
-                                        <p className='text-slate-700 font-medium'>Completions</p>
-                                        <div className='flex border border-slate-400 rounded-lg'>
-                                            <input placeholder='Min' className='flex w-full p-1 px-2 rounded-l-lg' />
-                                            <div className='border' />
-                                            <input placeholder='Max' className='flex w-full p-1 px-2 rounded-r-lg' />
-                                        </div>
-                                    </div>
-
-
-                                    <div className='flex w-full flex-col'>
-                                        <p className='text-slate-700 font-medium'>Type</p>
-                                        <div className='flex border border-slate-400 rounded-lg'>
-                                            <select name='type' className='flex w-full p-1 px-2 rounded-lg' />
-                                        </div>
-                                    </div>
-
-                                    <div className='flex w-full flex-col'>
-                                        <p className='text-slate-700 font-medium'>Completions</p>
-                                        <div className='flex border border-slate-400 rounded-lg'>
-                                            <input placeholder='Min' className='flex w-full p-1 px-2 rounded-l-lg' />
-                                            <div className='border' />
-                                            <input placeholder='Max' className='flex w-full p-1 px-2 rounded-r-lg' />
-                                        </div>
-                                    </div>
+                            <div className='flex flex-col w-full p-2 rounded-md items-center justify-center mt-2'>
+                                <div className='flex space-x-8'>
+                                    <p>Latitude: <b>{viewState.latitude.toFixed(4)}</b></p>
+                                    <p>Longitude: <b>{viewState.longitude.toFixed(4)}</b></p>
                                 </div>
-                            </div>)}
+                                <div className='border-b border-slate-300 flex w-2/3' />
+                            </div>
                         </div>
                     </div>
                 </div>
             </div >
-            <div id='HUD' className='flex w-full justify-center items-center absolute bottom-12 z-50' >
+            <div id='HUD' className='flex flex-col space-y-32 w-full justify-center items-center absolute bottom-12' >
+                <div className='flex flex-col space-y-2 items-center  p-3 rounded-xl'>
+                    <div
+                        className='flex flex-col w-full items-center'
+                    >
+                        <div className='flex flex-wrap justify-center gap-2 w-full'>
+                            {checkpoints.map((checkpoint, index) => (
+                                // <div className='rounded-full border shadow w-8 h-8 bg-slate-500' />
+                                <>
+                                    {
+                                        (index === 0) ?
+                                            // START
+                                            <div className='h-10 w-10 rounded-full bg-gradient-to-br from-lime-500/80 hover:from-lime-500/70 hover:to-lime-500/70 to-lime-500/80 border-2 border-lime-200/80 hover:border-lime-200/70 justify-center items-center'
+                                                onClick={() => setViewState({ latitude: checkpoint.lat, longitude: checkpoint.long })}
+                                            >
+                                                <div className='flex w-full justify-center h-full items-center text-md font-bold'>
+                                                    {index + 1}
+                                                </div>
+                                            </div>
+                                            :
+                                            (index === checkpoints.length - 1) ?
+                                                // FINISH
+                                                <div className='h-10 w-10 rounded-full bg-gradient-to-br from-yellow-500/80 hover:from-yellow-500/70 hover:to-yellow-500/70 to-yellow-500/80 border-2 border-yellow-200/80 hover:border-yellow-200/70 justify-center items-center'
+                                                    onClick={() => setViewState({ latitude: checkpoint.lat, longitude: checkpoint.long })}
+                                                >
+                                                    <div className='flex w-full justify-center h-full items-center text-md font-bold'>
+                                                        {index + 1}
+                                                    </div>
+                                                </div>
+                                                :
+                                                <div className='h-10 w-10 rounded-full bg-gradient-to-br from-sky-500/80 hover:from-blue-500/70 hover:to-sky-500/70 to-blue-500/80 border-2 border-blue-200/80 hover:border-sky-200/70 justify-center items-center'
+                                                    onClick={() => setViewState({ latitude: checkpoint.lat, longitude: checkpoint.long })}
+                                                >
+                                                    <div className='flex w-full justify-center h-full items-center text-md font-bold'>
+                                                        {index + 1}
+                                                    </div>
+                                                </div>
+                                    }
+                                </>
+                            ))}
+                        </div>
+                    </div>
+                </div>
                 <div className='flex w-full absolute justify-center space-x-2 items-center'>
                     <div className='flex space-x-1'>
                         {
                             (checkpoints.length === 0) ? (
                                 <div className='flex flex-col w-12 h-12 space-y-2 justify-center items-center bg-slate-200 p-3 rounded-xl border-2 border-slate-300 shadow text-2xl font-black
                                 opacity-50'
-                                    onClick={() => {
-                                        console.log('Deleting previous')
-                                        checkpoints.pop();
-                                    }}
                                 >
                                     <UndoIcon />
                                 </div>
@@ -253,8 +253,8 @@ export default function CreateMap() {
                                 :
                                 <div className='flex flex-col w-12 h-12 space-y-2 justify-center items-center bg-slate-200 p-3 rounded-xl border-2 border-slate-300 shadow text-2xl font-black'
                                     onClick={() => {
-                                        console.log('Deleting previous')
                                         checkpoints.pop();
+                                        console.log('Deleting previous Checkpoint -- UPDATED CHECKPOINTS ARRAY', checkpoints)
                                     }}
                                 >
                                     <UndoIcon />
@@ -279,6 +279,31 @@ export default function CreateMap() {
                     </div>
                 </div>
             </div>
+            {showIntro && (
+                <div id='intro' className='flex w-full justify-center items-center absolute h-full z-100 bg-black/80' >
+                    <div className='flex justify-center items-center h-full'>
+                        <div className='flex flex-col w-5/6 pb-4 space-y-4 justify-center items-center absolute bg-slate-200  rounded-xl border-2 shadow'>
+                            <img src={demo} className='object-contain p-1 rounded-xl flex h-full' alt='trail' />
+                            <div className='flex flex-col w-full justify-center items-center space-y-1 p-2'>
+                                <p className='flex w-full justify-center items-center text-2xl text-center font-medium'>
+                                    Welcome to Athleague!
+                                </p>
+                                <p className='flex w-full justify-center items-center text-md text-center font-normal'>
+                                    Create custom activity courses to establish easy goals for improving fitness.
+                                </p>
+                            </div>
+                            <div className='bg-blue-500 px-4 rounded-md text-xl text-slate-100 p-1'
+                                onClick={() => {
+                                    console.log('clicked')
+                                    setShowIntro(!showIntro)
+                                    console.log(showIntro)
+                                }}>
+                                Next
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </Map >
     )
 }

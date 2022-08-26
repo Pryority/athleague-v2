28499db359d115a8d4916155201285a23adfe79b
pryority <matthewapryor@gmail.com>
2022-08-26 19:19:03 -0400

    same as before

--- client/src/components/NFT/NFTCard.js
+++ client/src/components/NFT/NFTCard.js
@@ -1,69 +1,69 @@
-import { useEffect, useState } from "react";
-import { ethers } from 'ethers';
-import Contract from '../../artifacts/contracts/CourseTokens.sol/CourseTokens.json';
+// import { useEffect, useState } from "react";
+// import { ethers } from 'ethers';
+// import Contract from '../../artifacts/contracts/CourseTokens.sol/CourseTokens.json';
 
-const CONTRACT_ADDRESS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
-const contract = new ethers.Contract(CONTRACT_ADDRESS, Contract.abi, signer);
-
-
-function NFTCard({ tokenId, getCount }) {
-    const contentId = ''; // IPFS hash (example: QmS7g9gdD7976ggjdD78S4jN90G8dfds0llGoD8)
-    const metadataURI = `${contentId}/${tokenId}.json`;
-    const imageURI = `img/${tokenId}.png`
-
-    const [isMinted, setIsMinted] = useState(false);
-
-    const getMintedStatus = async () => {
-        const result = await contract.isContentOwned(metadataURI);
-        console.log(result)
-        setIsMinted(result);
-    };
-
-    const mintToken = async () => {
-        const connection = await contract.connect(signer);
-        const addr = connection.address;
-        const result = await contract.mintCourseToken(addr, metadataURI, { value: ethers.utils.parseEther('0.005') });
-
-        await result.wait();
-        getMintedStatus();
-    }
-
-    async function getURI() {
-        const uri = await contract.tokenURI(tokenId);
-    }
-
-    useEffect(() => {
-        getMintedStatus();
-    }, [isMinted])
+// const CONTRACT_ADDRESS = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
+// const provider = new ethers.providers.Web3Provider(window.ethereum);
+// const signer = provider.getSigner();
+// const contract = new ethers.Contract(CONTRACT_ADDRESS, Contract.abi, signer);
 
 
-    return (
-        <div className="flex flex-col space-y-4 items-center justify-center p-2 rounded-lg w-full bg-slate-400 ">
-            <div>Activity Token</div>
-            {!isMinted ? (
-                <button className='p-2 w-2/3 font-bold bg-lime-400 border-2 border-slate-600 rounded-lg'
-                    onClick={() => {
-                        console.log('hello world, minting a token')
-                        // mintToken();
+// function NFTCard({ tokenId, getCount }) {
+//     const contentId = ''; // IPFS hash (example: QmS7g9gdD7976ggjdD78S4jN90G8dfds0llGoD8)
+//     const metadataURI = `${contentId}/${tokenId}.json`;
+//     const imageURI = `img/${tokenId}.png`
 
-                    }}
-                >Mint</button>
-            ) :
-                (
-                    <button className='p-2 w-2/3 font-bold bg-lime-400 border-2 border-slate-600 rounded-lg'
-                        onClick={() => {
-                            console.log('hello world, minting a token')
-                            mintToken();
-                            tokenId++
-                        }}
-                    >Taken</button>
-                )
-            }
+//     const [isMinted, setIsMinted] = useState(false);
 
-        </div>
-    )
-}
+//     const getMintedStatus = async () => {
+//         const result = await contract.isContentOwned(metadataURI);
+//         console.log(result)
+//         setIsMinted(result);
+//     };
 
-export default NFTCard
+//     const mintToken = async () => {
+//         const connection = await contract.connect(signer);
+//         const addr = connection.address;
+//         const result = await contract.mintCourseToken(addr, metadataURI, { value: ethers.utils.parseEther('0.005') });
+
+//         await result.wait();
+//         getMintedStatus();
+//     }
+
+//     async function getURI() {
+//         const uri = await contract.tokenURI(tokenId);
+//     }
+
+//     useEffect(() => {
+//         getMintedStatus();
+//     }, [isMinted])
+
+
+//     return (
+//         <div className="flex flex-col space-y-4 items-center justify-center p-2 rounded-lg w-full bg-slate-400 ">
+//             <div>Activity Token</div>
+//             {!isMinted ? (
+//                 <button className='p-2 w-2/3 font-bold bg-lime-400 border-2 border-slate-600 rounded-lg'
+//                     onClick={() => {
+//                         console.log('hello world, minting a token')
+//                         // mintToken();
+
+//                     }}
+//                 >Mint</button>
+//             ) :
+//                 (
+//                     <button className='p-2 w-2/3 font-bold bg-lime-400 border-2 border-slate-600 rounded-lg'
+//                         onClick={() => {
+//                             console.log('hello world, minting a token')
+//                             mintToken();
+//                             tokenId++
+//                         }}
+//                     >Taken</button>
+//                 )
+//             }
+
+//         </div>
+//     )
+// }
+
+// export default NFTCard
--- client/src/components/User/WalletBalance.js
+++ client/src/components/User/WalletBalance.js
@@ -1,26 +1,26 @@
-import { useState } from 'react';
-import { ethers } from 'ethers';
+// import { useState } from 'react';
+// import { ethers } from 'ethers';
 
-function WalletBalance() {
-    const [balance, setBalance] = useState(null);
+// function WalletBalance() {
+//     const [balance, setBalance] = useState(null);
 
-    const getBalance = async () => {
-        const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const balance = await provider.getBalance(account);
-        setBalance(ethers.utils.formatEther(balance));
-    };
+//     const getBalance = async () => {
+//         const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//         const provider = new ethers.providers.Web3Provider(window.ethereum);
+//         const balance = await provider.getBalance(account);
+//         setBalance(ethers.utils.formatEther(balance));
+//     };
 
-    return (
-        <div id='wallet-balance'>
-            <div className='flex flex-col space-y-4'>
-                <h5>Balance: {balance}</h5>
-                <button className='p-2 px-4 rounded-lg border-2 border-slate-300 bg-blue-400  font-bold text-md'
-                    onClick={() => getBalance()}
-                >Show My Balance</button>
-            </div>
-        </div >
-    )
-};
+//     return (
+//         <div id='wallet-balance'>
+//             <div className='flex flex-col space-y-4'>
+//                 <h5>Balance: {balance}</h5>
+//                 <button className='p-2 px-4 rounded-lg border-2 border-slate-300 bg-blue-400  font-bold text-md'
+//                     onClick={() => getBalance()}
+//                 >Show My Balance</button>
+//             </div>
+//         </div >
+//     )
+// };
 
-export default WalletBalance
+// export default WalletBalance

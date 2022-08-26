9385d03cc75d25161852d2c3d854ec002a8eb375
pryority <matthewapryor@gmail.com>
2022-08-26 17:36:28 -0400

    fix: styling of course creation coordinates panel, added routing between home and create course

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import demo from '../../assets/images/demo.png'
 import recents from './RecentlyCreated.json'
 
@@ -11,35 +12,39 @@ function Home() {
                     <div className='h-3 w-3 bg-stone-50 rounded-full' />
                 </div>
                 <p>home</p>
-                <p>create</p>
+                <Link to="/create">create</Link>
                 <p>profile</p>
             </div>
             <p className='flex w-full justify-center items-center text-2xl text-center font-medium lg:text-4xl'>
                 Recently Created
             </p>
-            <div className='grid grid-cols-2 w-full justify-center items-center gap-2'>
+            <div className='grid grid-cols-1 w-full h-full justify-center items-center gap-2'>
                 {recents.map((recent, i) => (
-                    <div className='flex flex-col justify-center items-center rounded-xl bg-orange-400 border-2 border-stone-50'>
+                    <div className='flex flex-col justify-center items-center rounded-xl bg-orange-400 border-2 border-stone-50 relative h-full'>
                         <div className='flex w-full'>
-                            <img src={demo} className='object-contain h-full w-full rounded-xl border-b-2' alt='demo-image' />
+                            <img src={demo} className='object-fit h-full w-full absolute rounded-xl border-b-2' alt='demo-image' />
                         </div>
-                        <div className='flex w-full justify-center p-2'>
-                            <div className='flex flex-col md:flex-row w-full justify-center p-2 bg-purple-400'>
-                                <div className='flex flex-col w-full bg-green-400'>
-                                    {/* <img src={imageURL + `${i}.png`} alt='demo-image' /> */}
-                                    <div className='flex w-full'>
-                                        {/* <p className='text-end'>Checkpoints</p> */}
-                                        <p className='text-end w-full'>{recent.checkpoints.length} Waypoints</p>
+                        <div className='flex flex-col w-full h-full justify-end items-end p-1 z-50'>
+                            <div className='grid grid-cols-2 gap-1 w-4/6'>
+                                <div className='flex w-full justify-end'>
+                                    <div className=' bg-slate-800/60 rounded-full px-1'>
+                                        <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full'>{recent.checkpoints.length} Waypoints</p>
                                     </div>
-                                    <p className='text-end'>{recent.distance} km</p>
                                 </div>
-                                <div className='flex flex-col w-full bg-green-400'>
-                                    {/* <img src={imageURL + `${i}.png`} alt='demo-image' /> */}
-                                    <div className='flex w-full'>
-                                        {/* <p className='text-end'>Checkpoints</p> */}
-                                        <p className='text-end w-full'>Adventure</p>
+                                <div className='flex w-full justify-end'>
+                                    <div className=' bg-slate-800/60 rounded-full px-1'>
+                                        <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full capitalize'>{recent.type}</p>
                                     </div>
-                                    <p className='text-end'>By:  {recent.createdBy}</p>
+                                </div>
+                                <div className='flex w-full justify-end'>
+                                    <div className=' bg-slate-800/60 rounded-full px-1'>
+                                        <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full'>{recent.distance} km</p>
+                                    </div>
+                                </div>
+                                <div className='flex w-full justify-end'>
+                                    <div className=' bg-slate-800/60 rounded-full px-1'>
+                                        <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full'>{recent.createdBy}</p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -51,3 +56,23 @@ function Home() {
 }
 
 export default Home
+
+// 
+// <div className='flex flex-col md:flex-row w-full justify-center'>
+// {/* <div className='flex w-full'>
+// {/* <img src={imageURL + `${i}.png`} alt='demo-image' /> */}
+// <div className='flex w-full'>
+//     {/* <p className='text-end text-sm'>Checkpoints</p> */}
+//     <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full'>{recent.checkpoints.length} Waypoints</p>
+// </div>
+// <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full'>{recent.distance} km</p>
+// </div>
+// <div className='flex w-full'>
+// {/* <img src={imageURL + `${i}.png`} alt='demo-image' /> */}
+// <div className='flex w-full'>
+//     {/* <p className='text-end text-sm'>Checkpoints</p> */}
+//     <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full'>Adventure</p>
+// </div>
+// <p className='text-end text-[12px] text-stone-50 font-medium p-1 w-full truncate text-slate-800'>{recent.createdBy}</p>
+// </div>
+// </div> */}
--- client/src/components/Maps/CreateMap.js
+++ client/src/components/Maps/CreateMap.js
@@ -9,11 +9,12 @@ import trail from '../../assets/images/trail.png'
 import SaveAsIcon from '@mui/icons-material/SaveAs';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import CourseMap from '../Maps/CourseMap';
-
+import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import { NavigationControl } from 'react-map-gl';
 import { GeolocateControl } from 'react-map-gl';
 import { ScaleControl } from 'react-map-gl';
 import WalletBalance from '../User/WalletBalance';
+import { Link } from 'react-router-dom'
 
 const TOKEN = process.env.REACT_APP_MAPBOX;
 
@@ -205,18 +206,19 @@ export default function CreateMap() {
                 })
             }
 
-            <div id='filter-menu' className='flex w-full px-2 space-x-2 justify-center items-center absolute top-2 z-2' >
+            <div id='upper-HUD' className='flex w-full px-2 space-x-2 justify-start items-center absolute top-2 z-2' >
+                <div className='flex w-10 h-10 bg-slate-700 rounded-full items-center justify-center opacity-90 border-2 border-slate-100/30'
+                    onClick={() => setShowQuit(true)}
+                >
+                    <KeyboardReturnIcon style={{ color: '#ffffff' }} />
+                </div>
 
-                <div className='flex flex-col space-y-2 w-2/3 md:w-2/3 lg:w-1/2 items-center justify-center bg-slate-200/90 p-3 rounded-xl border-2 border-slate-800/30 shadow'>
+                <div className='flex flex-col space-y-2 w-3/5 md:w-2/5 lg:w-1/3 items-center justify-center bg-slate-200/90 rounded-xl border-2 border-slate-800/30 shadow'>
                     {/* <WalletBalance /> */}
                     <div
                         className='flex flex-col w-full items-center justify-center'
                     >
-                        {/* <div className='flex w-full space-x-2'>
-                            <input id='geocode-search' placeholder='Search by location' className='flex  flex-col w-full z-2 rounded-lg p-2 border-2 border-slate-400' />
-                        </div> */}
-
-                        <div className='flex flex-col w-full p-2 rounded-md items-center justify-center mt-2'>
+                        <div className='flex flex-col w-full p-2 rounded-md items-center justify-center'>
                             <div className='flex space-x-8'>
                                 <p className='text-center'>Latitude: <b>{viewState.latitude.toFixed(4)}</b></p>
                                 <p className='text-center'>Longitude: <b>{viewState.longitude.toFixed(4)}</b></p>
@@ -226,11 +228,6 @@ export default function CreateMap() {
                     </div>
 
                 </div>
-                {/* <div className='flex p-2 items-center justify-center bg-neutral-200/80 rounded-xl border-2 border-slate-100'
-                        onClick={() => setShowQuit(!showQuit)}
-                    >
-                        <ExitToAppIcon />
-                    </div> */}
             </div >
             <div id='HUD' className='flex flex-col space-y-2 w-full justify-around items-center absolute bottom-20' >
                 <div className='flex flex-wrap justify-center gap-2 w-5/6 p-2'>
@@ -347,7 +344,7 @@ export default function CreateMap() {
                 >
                     <div className='flex justify-center items-center h-full'>
                         <form id='save-course-form'
-                            className='flex flex-col lg:w-3/5 w-5/6 pb-4 space-y-4 justify-center items-center absolute bg-slate-200  rounded-xl border-2 p-2 shadow lg:scale-75 shadow-lg'>
+                            className='flex flex-col lg:w-3/5 w-5/6 pb-4 space-y-4 justify-center items-center absolute bg-slate-200  rounded-xl border-2 p-2 shadow lg:scale-75'>
                             <div className='flex flex-col w-full justify-center items-center space-y-4 p-2'>
                                 <div className='flex w-full justify-between items-center'>
                                     <p className='text-xl text-start font-extrabold lg:text-4xl'>
@@ -431,13 +428,9 @@ export default function CreateMap() {
                                     }}>
                                     Cancel
                                 </div>
-                                <div className='flex w-1/2 bg-red-500 rounded-md text-lg text-slate-100 p-1 justify-center items-center'
-                                    onClick={() => {
-                                        console.log('quit course creation')
-                                        setShowQuit(!setShowQuit)
-                                    }}>
+                                <Link to='/' className='flex w-1/2 bg-red-500 rounded-md text-lg text-slate-100 p-1 justify-center items-center'>
                                     Quit
-                                </div>
+                                </Link>
                             </div>
                         </form>
                     </div>
